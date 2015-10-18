040a195476305a80cf404fbbc9b8a862cc29533b
Josh Mills <josh@trueheart78.com>
2015-10-18 17:19:25 -0400

    Pizza exercise finished, as well as challenge 3

--- ruby/08-pizza-party/slices.rb
+++ ruby/08-pizza-party/slices.rb
@@ -9,38 +9,31 @@ class PizzaParty < Gosu::Window
     self.caption = "Pizza Party!"
 
     @font = Gosu::Font.new(30)
-    @pizza = 1
     @people = 4
+    @slices = 2
   end
 
   def draw
     @pizza_pic.draw(0,100,1)
     @font.draw("Number of people? #{@people}", 20, 0, 2)
-    @font.draw("Number of pizzas? #{@pizza}", 20, 20, 2)
-    if @slices > 0
-      message = "Each person gets #{@slices} slices"
+    @font.draw("Number of slices each? #{@slices}", 20, 20, 2)
+    if @pizza > 0
+      message = "Please order #{@pizza} pizzas"
     else
       message = "Warning: Party needs more pizza!"
     end
     @font.draw(message, 20, 60, 2)
-    if @slices > 0
-      @font.draw("#{@leftover} slices leftover", 20, 80, 2)
-    end
+    @font.draw("#{@leftover} slices leftover", 20, 80, 2)
   end
 
   def update
     close if button_down? Gosu::KbEscape or button_down? Gosu::KbQ
     @people += 1 if button_down? Gosu::KbUp
     @people -= 1 if @people > 1 and button_down? Gosu::KbDown
-    @pizza += 1 if button_down? Gosu::KbRight
-    @pizza -= 1 if @pizza > 1 and button_down? Gosu::KbLeft 
-    @slices = @pizza * 8 / @people
-    @leftover = 0
-    if @slices > 0
-      @leftover = (@pizza * 8) % @slices
-    end
-
+    @slices += 1 if button_down? Gosu::KbRight
+    @slices -= 1 if @slices > 1 and button_down? Gosu::KbLeft 
+    @pizza = @slices * @people / 8
+    @leftover = @slices * @people % 8
   end
-
 end
 PizzaParty.new.show
